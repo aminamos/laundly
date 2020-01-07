16942eeb7e682a4796dba7b51aa6a8e6d9275b9e
aminamos <26092352+aminamos@users.noreply.github.com>
2020-01-06 23:30:18 -0800

    add role

--- db/migrate/20191110054730_devise_create_users.rb
+++ db/migrate/20191110054730_devise_create_users.rb
@@ -3,7 +3,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :role_id, foreign_key: true
+      t.references :role
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_054730) do
+ActiveRecord::Schema.define(version: 2020_01_07_065349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2019_11_10_054730) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "address"
     t.string "phone"
@@ -42,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_054730) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.bigint "role_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -51,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_054730) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "availabilities", "stores"
